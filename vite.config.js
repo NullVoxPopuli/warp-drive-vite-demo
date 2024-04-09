0923b81b0494a80cd9ca213a20e4ffcba3726bdd
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-04-09 15:38:19 -0400

    Add @embroider/vite for ember support, but we're blocked on embroider's next release not being ready

---
+++ vite.config.js
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vite'
+import {
+  resolver,
+  // hbs,
+  // scripts,
+  // templateTag,
+  // optimizeDeps,
+  // compatPrebuild,
+  // assets,
+} from "@embroider/vite";
+import { resolve } from "path";
+// import { babel } from "@rollup/plugin-babel";
+
+export default defineConfig({
+  // esbuild: false,
+  cacheDir: resolve("node_modules", ".vite"),
+  plugins: [
+    // hbs(),
+    // templateTag(),
+    // scripts(),
+    resolver(),
+    // compatPrebuild(),
+    // assets(),
+
+    // babel({
+    //   babelHelpers: "runtime",
+
+    //   // this needs .hbs because our hbs() plugin above converts them to
+    //   // javascript but the javascript still also needs babel, but we don't want
+    //   // to rename them because vite isn't great about knowing how to hot-reload
+    //   // them if we resolve them to made-up names.
+    //   extensions: [".gjs", ".js", ".hbs", ".ts", ".gts"],
+    // }),
+  ],
+  // optimizeDeps: optimizeDeps(),
+  // server: {
+  //   port: 4200,
+  //   watch: {
+  //     ignored: ["!**/node_modules/.embroider/rewritten-app/**"],
+  //   },
+  // },
+  // publicDir: resolve(process.cwd(), "public"),
+  // build: {
+  //   outDir: resolve(process.cwd(), "dist"),
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(root, "index.html"),
+  //       ...(shouldBuildTests(mode)
+  //         ? { tests: resolve(root, "tests/index.html") }
+  //         : undefined),
+  //     },
+  //   },
+  // },
+})
